b04ecfae9caeddd12c64e6d0760f19b7e8171421
Aleksey Chumakov <chumakov.aleksey@gmail.com>
2021-04-29 13:19:06 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import ReactDOM from "react-dom";
 
-console.log('Start');
+console.info('Start');
 
 // class App extends React.Component {
 //     render() {
@@ -9,8 +9,21 @@ console.log('Start');
 //       }
 // }
 
-const App = () => {
-    return (<h1>Render ReactJS</h1>);
+const App = (props) => {
+    const ButtonClick = () => {
+        console.info('ButtonClick');
+        setCount(count + 1);
+        console.info(count);
+    }
+
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <h1>Render ReactJS {props.mytext} {count}</h1>
+            <button onClick={ButtonClick}>Click</button>
+        </div>
+    );
 }
 
-ReactDOM.render(<App name="app"/>, document.getElementById("app"));
+ReactDOM.render(<App name="app" mytext="+"/>, document.getElementById("app"));
