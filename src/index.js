4b6b2d6113fe581548cbbfe16a15c2b638ac38b5
Aleksey Chumakov <chumakov.aleksey@gmail.com>
2021-04-30 12:33:15 +0300

    Устранены замечения

--- src/index.js
+++ src/index.js
@@ -2,16 +2,10 @@ import React, { useEffect, useState, useCallback } from "react";
 import ReactDOM from "react-dom";
 console.info('Start');
 
-// class App extends React.Component {
-//     render() {
-//         return (<h1>Render ReactJS</h1>);
-//       }
-// }
-
 const App = (props) => {
     const [arr, setArr] = useState([]);
 
-    const ButtonClick = () => {
+    const buttonClick = () => {
         setArr([...arr,new Date().toLocaleTimeString()]);
         console.log(arr);
     }
@@ -23,10 +17,10 @@ const App = (props) => {
     return (
         <div>
             <h1>Render ReactJS {props.mytext} {arr[arr.length - 1]}</h1>
-            <button onClick={ButtonClick}>Click</button>
+            <button onClick={buttonClick}>Click</button>
             <div>{arrItems}</div>
         </div>
     );
 }
 
-ReactDOM.render(<App name="app" mytext="+"/>, document.getElementById("app"));
+ReactDOM.render(<App mytext="+"/>, document.getElementById("app"));
